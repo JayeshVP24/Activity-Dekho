b0c4be3b69b1741bb1b3f9a129e195e310b72ebf
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-01-03 11:41:47 +0530

    leaked key removed and secrets added

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,12 +7,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvyg0ymLOqs181XfDZ2VxvuAEqDcvZrOI",
-  authDomain: "aicte-diary.firebaseapp.com",
-  projectId: "aicte-diary",
-  storageBucket: "aicte-diary.appspot.com",
-  messagingSenderId: "944319415450",
-  appId: "1:944319415450:web:3e453ecb0786bde4f68b23"
+  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+   authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+   projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+   storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+   appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
