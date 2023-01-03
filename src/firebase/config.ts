c642c33c6d747a8cfaf70eb19f62f31afc7a2206
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-01-03 12:23:57 +0530

    leaked key removed and secrets added #4

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,21 +1,36 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-import {getAuth} from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-   authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-   projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-   storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-   appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+
+console.log("prod env ", process.env.NODE_ENV);
+let firebaseConfig = {};
+if (process.env.NODE_ENV === "production") {
+  console.log("here is the node env")
+  firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: "aicte-diary.firebaseapp.com",
+    projectId: "aicte-diary",
+    storageBucket: "aicte-diary.appspot.com",
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+} else {
+  firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: "aicte-diary.firebaseapp.com",
+    projectId: "aicte-diary",
+    storageBucket: "aicte-diary.appspot.com",
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firedb = getFirestore(app)
-export const auth = getAuth(app)
+export const firedb = getFirestore(app);
+export const auth = getAuth(app);
