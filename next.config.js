b9363f11038fce035122d79ee1e5c28de057347a
JayeshVP24 <jayeshpotlabattini@gmail.com>
2023-01-11 21:36:42 +0530

    some issue with build probably solved #2

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       // Dangerously allow production builds to successfully complete even if
       // your project has type errors.
       // !! WARN !!
-      ignoreBuildErrors: false,  
+      ignoreBuildErrors: true,  
     },
     distDir: 'build'
   }
